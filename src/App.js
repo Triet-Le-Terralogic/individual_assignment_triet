fec45a8f79fb2bc1ea112127e735a26c9da7d44f
INI\triet.le <triet.le@terralogic.com>
2020-07-22 09:37:43 +0700

    Add 'Logo' Component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import FormWrapper from "./components/FormWrapper";
 import emaiIcon from "./assets/img/email_icon.svg";
 import keyIcon from "./assets/img/key_icon.svg";
 import CheckBox from "./components/CheckBox";
-import Logo from "./components/Logo";
 
 function App() {
   const formData = [
@@ -40,7 +39,6 @@ function App() {
   ];
   return (
     <div className="App">
-      <Logo />
       <FormWrapper
         formTitle="Login your account"
         formData={formData}
